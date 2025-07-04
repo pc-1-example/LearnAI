c230b7ec6454aec4bac03c2b35132c38c09e6dee
ReNothing <pashasob2009@gmail.com>
2025-07-04 15:02:35 +0300

    face_analysis

--- face/Тест#1/main.py
+++ face/Тест#1/main.py
@@ -4,6 +4,8 @@
 from deepface import DeepFace
 import json
 
+
+
 def face_verification(img_1, img_2):
     try:
         result_dict = DeepFace.verify(img1_path=img_1, img2_path=img_2)
@@ -19,15 +21,38 @@ def face_verification(img_1, img_2):
     except Exception as _ex:
         return _ex
     
+
+
 def face_recognition():
     try:
-        result = DeepFace.find(img_path="faces/1sam.jpg", db_path="faces/2sam.jpg")
+        #result = DeepFace.find(img_path="faces/jaki.jpg", db_path="jeki")
+        result = DeepFace.find(img_path="faces/2sam.jpg", db_path="jeki")
+        result = result.values.tolist()
+
+        return result
+    
+    except Exception as _ex:
+        return _ex
+
+
+
+def face_analysis():
+    try:
+        result_dict = DeepFace.analyze(img_path="faces/2sam.jpg", actions=['age', 'gender', 'race', 'emotion'])
+
+        with open('face_analysis.json', 'w') as file:
+           json.dump(result_dict, file, indent=4, ensure_ascii=False)
+
+        return result_dict
+    
     except Exception as _ex:
         return _ex
 
 def main():
     # print(face_verification(img_1="faces/yan.jpg", img_2="faces/3model.jpg"))
-    print(face_verification(img_1="faces/1sam.jpg", img_2="faces/2sam.jpg"))
+    # print(face_verification(img_1="faces/1sam.jpg", img_2="faces/2sam.jpg"))
+    # print(face_recognition())
+    print(face_analysis())
 
 if __name__ == "__main__":
     main()
