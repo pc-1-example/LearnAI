9e89907561222869409d7d57d198aab40a998d14
ReNothing <pashasob2009@gmail.com>
2025-07-04 14:14:59 +0300

    Add face verification images and update main.py logic
    Added four new face images and a result.json output file. Updated main.py to write verification results to result.json and return a user-friendly message in Russian. Also added a stub for face recognition and adjusted the main function to use new image paths.

--- face/Тест#1/main.py
+++ face/Тест#1/main.py
@@ -8,12 +8,26 @@ def face_verification(img_1, img_2):
     try:
         result_dict = DeepFace.verify(img1_path=img_1, img2_path=img_2)
 
-        return result_dict
+        with open('result.json', 'w') as file:
+            json.dump(result_dict, file, indent=4, ensure_ascii=False)
+
+        if result_dict.get("verified"):
+            return "Лица совпадают"
+        return "Лица не совпадают"
+
+        # return result_dict
     except Exception as _ex:
         return _ex
     
+def face_recognition():
+    try:
+        result = DeepFace.find(img_path="faces/1sam.jpg", db_path="faces/2sam.jpg")
+    except Exception as _ex:
+        return _ex
+
 def main():
-    print(face_verification(img_1="face/Тест#1/faces/yan.jpg", img_2="face/Тест#1/faces/3model.jpg"))
+    # print(face_verification(img_1="faces/yan.jpg", img_2="faces/3model.jpg"))
+    print(face_verification(img_1="faces/1sam.jpg", img_2="faces/2sam.jpg"))
 
 if __name__ == "__main__":
     main()
