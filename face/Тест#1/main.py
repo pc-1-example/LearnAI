9fac8be49c5cef161a6c91115f69933ae59a02a6
ReNothing <pashasob2009@gmail.com>
2025-07-04 14:03:22 +0300

    Fix

--- face/Тест#1/main.py
+++ face/Тест#1/main.py
@@ -2,6 +2,7 @@
 #   pip install tf-keras -- если выдает отшибку, то дополнительно утснови эту библиотеку
 
 from deepface import DeepFace
+import json
 
 def face_verification(img_1, img_2):
     try:
@@ -12,7 +13,7 @@ def face_verification(img_1, img_2):
         return _ex
     
 def main():
-    face_verification(img_1="faces/yan.jpg", img_2="faces/3model.jpg")
+    print(face_verification(img_1="faces/yan.jpg", img_2="faces/3model.jpg"))
 
 if __name__ == "__main__":
     main()
