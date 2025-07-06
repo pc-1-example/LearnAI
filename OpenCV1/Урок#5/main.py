2383f93acacf2dfd186127a4037383c2b4f15c30
ReNothing <pashasob2009@gmail.com>
2025-07-06 15:48:53 +0300

    Урок 5 по OpenCV завершен

--- OpenCV1/Урок#5/main.py
+++ OpenCV1/Урок#5/main.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('img.png')
+img = cv2.imread('img1.png')
 img = cv2.resize(img, (700, 700)) #Для удобства просмотра
 
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -8,9 +8,10 @@ img = cv2.resize(img, (700, 700)) #Для удобства просмотра
 # img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 # img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 r, g, b = cv2.split(img)
+img = cv2.merge([r, g, b])
 
-cv2.imshow('Image', r)
+cv2.imshow('Image', img)
 cv2.waitKey(0)
