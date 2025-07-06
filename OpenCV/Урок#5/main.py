e9607c793063d28f30280590cf6c6eafecafbf18
ReNothing <pashasob2009@gmail.com>
2025-07-06 15:36:06 +0300

    BGR2LAB

--- OpenCV/Урок#5/main.py
+++ OpenCV/Урок#5/main.py
@@ -1,19 +1,14 @@
 import cv2
 
+img = cv2.imread('img.png')
+img = cv2.resize(img, (700, 700)) #Для удобства просмотра
 
-cap = cv2.VideoCapture("videos/main.mp4")
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-while True:
-    success, img = cap.read()
-    cv2.imshow('Result', img)
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+# img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
 
-    img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
-    img = cv2.GaussianBlur(img, (9, 9), 0)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    img = cv2.Canny(img, 200, 200)
 
-    kernel = 
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+cv2.imshow('Image', img)
+cv2.waitKey(0)
