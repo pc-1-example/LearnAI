3deac4466c29696de19ff3c5b9c841c4555e3a9e
ReNothing <pashasob2009@gmail.com>
2025-07-20 21:28:04 +0300

    code

--- OpenCV1/Lesson#8/main.py
+++ OpenCV1/Lesson#8/main.py
@@ -12,7 +12,32 @@ edges = cv2.Canny(img_filter, 30, 200)
 
 cont = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cont = imutils.grab_contours(cont)
-cont = sorted(cont, key=cv2.contourArea, reverse=True)[:10]
+cont = sorted(cont, key=cv2.contourArea, reverse=True)[:8]
 
-pl.imshow(cv2.cvtColor(edges, cv2.COLOR_BGR2RGB))
+pos = None
+for c in cont:
+    approx = cv2.approxPolyDP(c, 10, True)
+    if len(approx) == 4:
+        pos = approx
+        break
+
+mask = np.zeros(gray.shape, np.uint8)
+new_img = cv2.drawContours(mask, [pos], 0, 255, -1)
+bitwise_img = cv2.bitwise_and(img, img, mask=mask)
+
+(x, y) = np.where(mask==255)
+(x1, y1) = np.min(x), np.min(y)
+(x2, y2) = np.max(x), np.max(y)
+cropp = gray[x1:x2, y1:y2]
+
+text = easyocr.Reader(['en'])
+text = text.readtext(cropp)
+print(text)
+
+res = text[0][-2]
+final_image = cv2.putText(img, res, (x1 - 200, y2 + 160), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+final_image = cv2.rectangle(img, (x1, x2), (y1, y2), (0, 255, 0), 2)
+
+
+pl.imshow(cv2.cvtColor(final_image, cv2.COLOR_BGR2RGB))
 pl.show()
