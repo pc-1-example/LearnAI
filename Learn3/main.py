bb21921e8e5c2fff582ce4f89f868026644f8a67
ReNothing <pashasob2009@gmail.com>
2025-07-22 08:50:56 +0300

    Добавить модель нейронной сети и обновить тайм-коды в видео

--- Learn3/main.py
+++ Learn3/main.py
@@ -10,5 +10,55 @@ from tensorflow.keras.models import Sequential
 
 dataset_dir = pathlib.Path("Learn3/dataset/flower_photos")
 
-image_count = len(list(dataset_dir.glob('*/*.jpg')))
-print(f"Total images: {image_count}")
+batch_size = 32
+img_wight = 180
+img_height = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    dataset_dir,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_wight),
+    batch_size=batch_size,
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    dataset_dir,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_wight),
+    batch_size=batch_size,
+)
+
+class_names = train_ds.class_names
+print(f"Class names: {class_names}")
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+num_classes = len(class_names)
+model = Sequential([
+    layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_wight, 3)),
+
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+
+    layers.flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
+])
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy']
+)
+
+model.summary()
