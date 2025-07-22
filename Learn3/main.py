727a675c8fa2e2baa2bae739ddf443864c230770
ReNothing <pashasob2009@gmail.com>
2025-07-22 16:24:36 +0300

    fix error

--- Learn3/main.py
+++ Learn3/main.py
@@ -44,7 +44,7 @@ val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 num_classes = len(class_names)
 model = Sequential([
-    layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+    layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
 
     layers.Conv2D(16, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
